2a4cc97208a312946e5fca13136cc7b0e8eed9f6
Meghna Gupta <meghnagupta64@gmail.com>
2019-10-13 14:47:43 +0530

    Help Centre and Complaint Page added

--- app/src/main/java/com/example/dmrcconnect/MainComplaintFragment.java
+++ app/src/main/java/com/example/dmrcconnect/MainComplaintFragment.java
@@ -8,44 +8,30 @@ import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
-public class HelpCentreFragment extends Fragment {
+public class MainComplaintFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_helpcentre, container, false);
+        return inflater.inflate(R.layout.fragment_complaint_main, container, false);
 
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        CardView complaint_card = (CardView) view.findViewById(R.id.complaint_card);
-        complaint_card.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainComplaintFragment fragment = new MainComplaintFragment();
-                getActivity().getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.fragment_container, fragment, "findThisFragment")
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });
-
         ImageView back_button = getActivity().findViewById(R.id.back_button);
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HomeFragment fragment = new HomeFragment();
+                HelpCentreFragment fragment = new HelpCentreFragment();
                 getActivity().getSupportFragmentManager().beginTransaction()
                         .replace(R.id.fragment_container, fragment, "findThisFragment")
                         .addToBackStack(null)
                         .commit();
             }
         });
-
     }
 }
