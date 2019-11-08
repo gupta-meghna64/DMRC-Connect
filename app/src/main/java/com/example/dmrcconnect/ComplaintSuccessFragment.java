0ba960f225caf1f16fdab489e5403b892277dee6
Saatvik Jain <saatvik16261@iiitd.ac.in>
2019-11-09 01:08:43 +0530

    Form completed, some errors removed.

--- app/src/main/java/com/example/dmrcconnect/ComplaintSuccessFragment.java
+++ app/src/main/java/com/example/dmrcconnect/ComplaintSuccessFragment.java
@@ -1,20 +1,15 @@
 package com.example.dmrcconnect;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatAutoCompleteTextView;
-import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 
